49d0977f17aa6d8c99ef4941b1f523594fbaa996
feigetiantian@163.com <feigetiantian@163.com>
2025-07-18 19:04:31 +0800

    修改一下适配kotlin的代码

--- android/src/main/kotlin/com/rhyme/r_album/RAlbumPlugin.kt
+++ android/src/main/kotlin/com/rhyme/r_album/RAlbumPlugin.kt
@@ -1,6 +1,5 @@
 package com.rhyme.r_album
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.Intent.ACTION_MEDIA_SCANNER_SCAN_FILE
@@ -13,33 +12,26 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.io.FileInputStream
 import java.lang.Exception
 import kotlin.concurrent.thread
 
 /** RAlbumPlugin */
-const val methodName: String = "com.rhyme_lph/r_album"
-var context: Context? = null
-
-public class RAlbumPlugin : FlutterPlugin, MethodCallHandler {
+class RAlbumPlugin : FlutterPlugin, MethodCallHandler {
+    private lateinit var channel: MethodChannel
+    private var context: Context? = null
     private val handler: Handler = Handler()
+
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         context = flutterPluginBinding.applicationContext
-        val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), methodName)
-        channel.setMethodCallHandler(RAlbumPlugin())
-
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "com.rhyme_lph/r_album")
+        channel.setMethodCallHandler(this)
     }
 
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            context = registrar.activity()
-
-            val channel = MethodChannel(registrar.messenger(), methodName)
-            channel.setMethodCallHandler(RAlbumPlugin())
-        }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+        context = null
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -47,65 +39,6 @@ public class RAlbumPlugin : FlutterPlugin, MethodCallHandler {
             "createAlbum" -> createAlbum(call, result)
             "saveAlbum" -> saveAlbum(call, result)
             else -> result.notImplemented()
-        }
-    }
-
-    private fun saveAlbum(call: MethodCall, result: Result) {
-
-        val albumName = call.argument<String>("albumName")
-        val filePaths = call.argument<List<String>>("filePaths")
-        if (albumName == null) {
-            result.error("100", "albumName is not null", null)
-            return
-        }
-        if (filePaths == null) {
-            result.error("101", "filePaths is not null", null)
-            return
-        }
-        thread {
-            val rootFile = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), albumName)
-            if (!rootFile.exists()) {
-                rootFile.mkdirs()
-            }
-
-            val resultPaths = mutableListOf<String>()
-
-            try {
-                for (path in filePaths) {
-                    val fileName: String = if (path.lastIndexOf('.') == -1) {
-                        "${System.currentTimeMillis()}"
-                    } else {
-                        val suffix: String = path.substring(path.lastIndexOf(".") + 1)
-                        "${System.currentTimeMillis()}.$suffix"
-                    }
-                    val itemFile = File(rootFile, fileName)
-                    if (!itemFile.exists()) itemFile.createNewFile()
-                    val outPut = itemFile.outputStream()
-                    val inPut = FileInputStream(path)
-                    val buf = ByteArray(1024)
-                    var len = 0
-                    while (true) {
-                        len = inPut.read(buf)
-                        if (len == -1) break
-                        outPut.write(buf, 0, len)
-                    }
-                    outPut.flush()
-                    outPut.close()
-
-                    inPut.close()
-                    resultPaths.add(itemFile.absolutePath)
-                    handler.post {
-                        context!!.sendBroadcast(Intent(ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(itemFile)))
-                    }
-                }
-                handler.post {
-                    result.success(true)
-                }
-            } catch (e: Exception) {
-                handler.post {
-                    result.success(false)
-                }
-            }
         }
     }
 
@@ -126,8 +59,54 @@ public class RAlbumPlugin : FlutterPlugin, MethodCallHandler {
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        context = null
+    private fun saveAlbum(call: MethodCall, result: Result) {
+        val albumName = call.argument<String>("albumName")
+        val filePaths = call.argument<List<String>>("filePaths")
 
+        if (albumName == null) {
+            result.error("100", "albumName is not null", null)
+            return
+        }
+        if (filePaths == null) {
+            result.error("101", "filePaths is not null", null)
+            return
+        }
+
+        thread {
+            val rootFile = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), albumName)
+            if (!rootFile.exists()) {
+                rootFile.mkdirs()
+            }
+
+            try {
+                for (path in filePaths) {
+                    val fileName = if (path.lastIndexOf('.') == -1) {
+                        "${System.currentTimeMillis()}"
+                    } else {
+                        val suffix = path.substring(path.lastIndexOf(".") + 1)
+                        "${System.currentTimeMillis()}.$suffix"
+                    }
+                    val itemFile = File(rootFile, fileName)
+                    if (!itemFile.exists()) itemFile.createNewFile()
+                    val output = itemFile.outputStream()
+                    val input = FileInputStream(path)
+                    val buf = ByteArray(1024)
+                    var len: Int
+                    while (input.read(buf).also { len = it } != -1) {
+                        output.write(buf, 0, len)
+                    }
+                    output.flush()
+                    output.close()
+                    input.close()
+
+                    handler.post {
+                        context?.sendBroadcast(Intent(ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(itemFile)))
+                    }
+                }
+                handler.post { result.success(true) }
+            } catch (e: Exception) {
+                handler.post { result.success(false) }
+            }
+        }
     }
 }
